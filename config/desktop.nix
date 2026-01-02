a26c25d27fc2289a706444bbacc129452ff37e29
wiemanboy <wiemanboy@gmail.com>
2026-01-02 21:15:07 +0100

    FIX: warning

--- config/desktop.nix
+++ config/desktop.nix
@@ -11,8 +11,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   programs.dconf.profiles.user.databases = [{
     settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
